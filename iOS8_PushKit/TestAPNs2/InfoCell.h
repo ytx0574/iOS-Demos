bf0ed72287b3c84fb22eb81c21423060b54b37b4
Johnson-Mini <ytx0573@gmail.com>
2017-05-18 15:41:31 +0800

    iOS8 PushKit Demo提交

---
+++ iOS8_PushKit/TestAPNs2/InfoCell.h
@@ -0,0 +1,13 @@
+//
+//  InfoCell.h
+//  Noti
+//
+//  Created by Johnson on 2017/5/12.
+//  Copyright © 2017年 Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *labelInfo;
+@end
